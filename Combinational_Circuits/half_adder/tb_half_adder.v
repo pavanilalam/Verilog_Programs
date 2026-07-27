2b731c9cb032afc4689bcb9809419358bca0959e
pavanilalam <pavanilalam21@gmail.com>
2026-07-27 20:46:55 +0530

    Add testbench for half adder module

---
+++ Combinational_Circuits/half_adder/tb_half_adder.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps
+
+module tb_half_adder;
+
+reg a, b;
+wire sum, carry;
+
+half_adder uut (
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+);
+
+initial begin
+    $display("Time\tA\tB\tSUM\tCARRY");
+    $monitor("%0t\t%b\t%b\t%b\t%b",
+             $time, a, b, sum, carry);
+
+    a = 0; b = 0; #10;
+    a = 0; b = 1; #10;
+    a = 1; b = 0; #10;
+    a = 1; b = 1; #10;
+
+    $finish;
+end
+
+endmodule
