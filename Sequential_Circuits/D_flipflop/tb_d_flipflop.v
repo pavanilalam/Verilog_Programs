fc02a3a9accde6a93260ba543a02a9e6b33bedc4
pavanilalam <pavanilalam21@gmail.com>
2026-07-29 20:34:22 +0530

    Create tb_d_flipflop.v

---
+++ Sequential_Circuits/D_flipflop/tb_d_flipflop.v
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+
+module tb_d_flipflop;
+
+reg clk, d;
+wire q;
+
+d_flipflop uut (
+    .clk(clk),
+    .d(d),
+    .q(q)
+);
+
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+
+    d = 0; #10;
+    d = 1; #10;
+    d = 0; #10;
+    d = 1; #10;
+
+    $finish;
+end
+
+endmodule
