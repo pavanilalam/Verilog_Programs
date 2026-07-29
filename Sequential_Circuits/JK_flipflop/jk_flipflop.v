46d2b6828b747b8e48cc53a235f4fd8505071d62
pavanilalam <pavanilalam21@gmail.com>
2026-07-29 20:54:29 +0530

    Create jk_flipflop.v

---
+++ Sequential_Circuits/JK_flipflop/jk_flipflop.v
@@ -0,0 +1,18 @@
+module jk_flipflop (
+    input clk,
+    input j,
+    input k,
+    output reg q
+);
+
+always @(posedge clk)
+begin
+    case ({j, k})
+        2'b00: q <= q;
+        2'b01: q <= 1'b0;
+        2'b10: q <= 1'b1;
+        2'b11: q <= ~q;
+    endcase
+end
+
+endmodule
